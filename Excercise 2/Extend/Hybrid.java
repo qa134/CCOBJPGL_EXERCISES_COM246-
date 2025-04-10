0bcb4bb3375c6c031d63a5390c7e5c189cb94c8f
John Paul Orcine <johnpaulorcine40@gmail.com>
2025-04-10 14:27:03 +0800

    Add files via upload

---
+++ Excercise 2/Extend/Hybrid.java
@@ -0,0 +1,21 @@
+public class Hybrid {
+    protected String hybridvehicle;
+    protected String electric;
+    protected String combustion;
+
+    public String getvehicle1() {
+        return this.vehicle1;
+    }
+
+    public String getvehicle2() {
+        return this.vehicle2;
+    }
+
+    public String getelectric() {
+        return this.electric;
+    }
+
+    public String getcombustion() {
+        return this.combustion;
+    }
+}
